175e2b7d3c1f59f1c2ebb537c50be1edbb0e3669
Karl Klashinsky <klash@vmware.com>
2020-09-24 10:32:03 -0700

    Various minor improvements for use in VMware
    - Aliases and wrappers for p4 commands
    - Tweaks to make source highlighting work
    - Add tmux config file

---
+++ bin/src-hilite-lesspipe.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+HIGHLIGHT_STYLE_FILE=${HIGHLIGHT_STYLE_FILE:-$HOME/.klash.style}
+
+for source in "$@"; do
+    case $source in
+	*ChangeLog|*changelog) 
+        source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=$HIGHLIGHT_STYLE_FILE -i "$source" ;;
+	*Makefile|*makefile) 
+        source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=$HIGHLIGHT_STYLE_FILE -i "$source" ;;
+        *) source-highlight --failsafe --infer-lang -f esc --style-file=$HIGHLIGHT_STYLE_FILE -i "$source" ;;
+    esac
+done
