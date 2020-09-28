2e807b28a75e527fbb3b9b98409000e67baccfd0
Karl Klashinsky <klash@vmware.com>
2020-09-28 10:05:56 -0700

    Use python lang for looking at *.sc files
    Have source highlighting work for scons' foo.sc files. These are
    python files, so just use python language def.

--- bin/src-hilite-lesspipe.sh
+++ bin/src-hilite-lesspipe.sh
@@ -8,6 +8,8 @@ for source in "$@"; do
         source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=$HIGHLIGHT_STYLE_FILE -i "$source" ;;
 	*Makefile|*makefile) 
         source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=$HIGHLIGHT_STYLE_FILE -i "$source" ;;
+	*.sc)
+        source-highlight --failsafe -f esc --lang-def=python.lang --style-file=$HIGHLIGHT_STYLE_FILE -i "$source" ;;
         *) source-highlight --failsafe --infer-lang -f esc --style-file=$HIGHLIGHT_STYLE_FILE -i "$source" ;;
     esac
 done
